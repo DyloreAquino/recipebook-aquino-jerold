d2587f34cc31e5a21dd85cd5aedab730673e2df8
DyloreAquino <lutherjerold@gmail.com>
2025-02-27 23:35:49 +0800

    Added admin panel for Recipe for Bonus points. Unable to do Inline Admin for now.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    """Create RecipeIngredient inline for RecipeAdmin."""
+
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    """Create admin panel for Recipe."""
+
+    model = Recipe
+    inlines = [RecipeIngredientInline,]
+
+
+admin.site.register(Recipe, RecipeAdmin)
