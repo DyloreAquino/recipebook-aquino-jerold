9e6667d56f273549f905d3f92321b3994cb39f29
DyloreAquino <lutherjerold@gmail.com>
2025-03-10 14:34:53 +0800

    Start new app named 'accounts' for project wide account access

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, 'the field must contain more than 255 characters')
+        ]
+    )
