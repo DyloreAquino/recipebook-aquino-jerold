a5e7bccc3c1a690a91ce749b465f286c229a9c4a
DyloreAquino <lutherjerold@gmail.com>
2025-03-10 21:57:38 +0800

    Modified login and logout urls

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 # Login related files
 
 LOGIN_REDIRECT_URL = '/recipes/list'
-LOGOUT_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Default primary key field type
