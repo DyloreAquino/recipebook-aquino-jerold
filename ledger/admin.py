c8f00f942ca990e028b22435399030d63e6c65ab
DyloreAquino <lutherjerold@gmail.com>
2025-03-10 15:02:09 +0800

    Added admin panel for Ingredient

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Ingredient
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -14,4 +14,11 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline,]
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    """Create Ingredient admin panel."""
+
+    model = Ingredient
+
+
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
