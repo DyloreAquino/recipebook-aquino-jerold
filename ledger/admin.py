a213b61ddafb83f3b33c94ec9f87585e2f4d8750
DyloreAquino <lutherjerold@gmail.com>
2025-03-24 18:31:58 +0800

    Created RecipeImage inline, Fixed Recipe User field in Recipe model

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     """Create RecipeIngredient inline for RecipeAdmin."""
 
     model = RecipeIngredient
+    extra = 1
+
+
+class RecipeImageInline(admin.TabularInline):
+    """Create RecipeImage inline for RecipeAdmin."""
+
+    model = RecipeImage
+    extra = 1
 
 
 class RecipeAdmin(admin.ModelAdmin):
     """Create admin panel for Recipe."""
 
-    inlines = [RecipeIngredientInline,]
+    inlines = [RecipeIngredientInline, RecipeImageInline,]
 
 
 class IngredientAdmin(admin.ModelAdmin):
