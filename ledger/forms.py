c453a1a3b1110b6522aea377ad194016ea3d7ff3
DyloreAquino <lutherjerold@gmail.com>
2025-03-24 21:25:53 +0800

    Added a class to add images through a form

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,22 +1,18 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 
 class RecipeForm(forms.ModelForm):
+    """A class for creating recipes through a form."""
+
     class Meta:
         model = Recipe
         fields = '__all__'
-        """
-        widgets = {
-            'created_on' : forms.TextInput(
-                attrs={
-                    'type' :'datetime-local'
-                }
-            ),
-            'updated_on' : forms.TextInput(
-                attrs={
-                    'type' :'datetime-local'
-                }
-            )
-        } 
-        """
+
+
+class RecipeImageForm(forms.ModelForm):
+    """A class for adding images to recipes through a form."""
+
+    class Meta:
+        model = RecipeImage
+        fields = '__all__'
